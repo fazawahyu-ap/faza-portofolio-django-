29bcceff6b05308bae17441892ab263ee784b260
Faza Wahyu Adi Putra <fazawahyu10@gmail.com>
2025-11-30 09:10:52 +0700

    Fix settings.py for Vercel deployment

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,6 @@ USE_TZ = True
 # STATIC FILES (CSS/JS/IMG)
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
